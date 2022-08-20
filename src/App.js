9d7ded9bc5e9bc558d1c2f51532d4f6c3842f04c
adamman722 <tarpeyadam@gmail.com>
2022-08-20 12:39:03 -0400

    Added styled components and sprites

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styles/App.css";
 import { useGetAllPokemonByNameQuery } from "./services/api/pokemon";
 import { Outlet } from "react-router-dom";
+import { listOfTrainers } from "./trainerList";
 
 function App() {
   const { data, error, isLoading } = useGetAllPokemonByNameQuery("bulbasaur");
@@ -8,7 +9,11 @@ function App() {
   // console.log(data);
   // console.log(error);
   // console.log(isLoading);
-
+  //below is how we are going to import sprites
+  //<img src={listOfTrainers[0].img} alt="" />
+  const trainerSprites = listOfTrainers.map((trainer) => {
+    return <img src={trainer.img} alt="" />;
+  });
   return (
     <div className="App">
       <Outlet />
