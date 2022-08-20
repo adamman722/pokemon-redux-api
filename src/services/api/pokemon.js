362bb9d8a95b02530986eca8c80979dcab3cddeb
adamman722 <tarpeyadam@gmail.com>
2022-08-19 21:43:12 -0400

    Start of pokemon api reduxAPI

---
+++ src/services/api/pokemon.js
@@ -0,0 +1,17 @@
+// Need to use the React-specific entry point to allow generating React hooks
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// Define a service using a base URL and expected endpoints
+export const pokemonApi = createApi({
+  reducerPath: "pokemonApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
+  endpoints: (builder) => ({
+    getAllPokemonByName: builder.query({
+      query: () => `pokemon/?limit=900`,
+    }),
+  }),
+});
+
+// Export hooks for usage in function components, which are
+// auto-generated based on the defined endpoints
+export const { useGetAllPokemonByNameQuery } = pokemonApi;
